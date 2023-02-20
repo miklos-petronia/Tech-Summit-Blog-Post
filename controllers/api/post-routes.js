d07a4f80a4710964e0b7cfea1fe9538e33245f43
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-20 19:36:10 +0100

    updating the post application

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -15,3 +15,23 @@ router.post('/', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// UPDATing the POST
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        console.log('here is the req.body', req.body);
+        const [affectedRows] = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
