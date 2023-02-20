ab8a1a696711a045dc8289713f9174187708a34b
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-20 19:24:17 +0100

    arranging the data information of comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -7,3 +7,31 @@ router.get('/', withAuth, async (req, res) => {
         const commentData = await Comment.findAll({
             include: [User],
         });
+        
+        
+    // Arrange the data information
+        const comments = commentData.map((comment) => comment.get({ plain: true }));
+
+        console.log(comments);
+
+        res.render('single-post', { comments, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+
+    try {
+        const newComment = await Comment.create({
+            ...body,
+            userId: req.session.userId,
+        });
+        res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
