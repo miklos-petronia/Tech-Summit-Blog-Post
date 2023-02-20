3cb2ed5b0225182a0f05792fcf65eb6b8998eda7
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-20 19:34:36 +0100

    developing the router.post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
+
+// Developing the POST
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+    console.log(body);
+    try {
+        const newPost = await Post.create({ ...body, userId: req.session.userId });
+        console.log("Here is the new post: ", newPost);
+        res.json(newPost);
+    } catch (err) {
+        console.log('IT FAILED!', err);
+        res.status(500).json(err);
+    }
+});
