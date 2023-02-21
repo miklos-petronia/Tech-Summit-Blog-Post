97feb821dbeef34baf18eb288d8caa211e1a1934
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-21 01:22:04 +0100

    login application

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -21,3 +21,36 @@ router.post('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+// LOGIN in application
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'No user account found!' });
+            return;
+        }
+
+        const validPassword = user.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'No user account found!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'No user account found!' });
+    }
+});
