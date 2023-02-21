48d658bc565a3ccdbbc04d3011f71f6d2f0dbff9
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-21 01:42:34 +0100

    POST Dashboard where the database information find all from the post model

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 const { Post, User } = require('../models/');
 const withAuth = require('../utils/auth');
+
+// ALL POSTS Control Panel
+router.get('/', withAuth, async (req, res) => {
+    try {
+        // Store the results application of the database information query in the variable "postData". This should use something that "finds all" from the Post model. This might need a where clause!
+
+        const postData = await Post.findAll({
+            where: { "userId": req.session.userId },
+            include: [User]
+        });
