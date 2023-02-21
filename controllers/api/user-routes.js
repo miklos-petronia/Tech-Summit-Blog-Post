77f73444b72378f328a4f36ed4679a2b6eb8d5b2
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-02-21 01:22:59 +0100

    logout application

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -54,3 +54,17 @@ router.post('/login', async (req, res) => {
         res.status(400).json({ message: 'No user account found!' });
     }
 });
+
+// LOGOUT Application
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
